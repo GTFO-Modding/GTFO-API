1695d0d09d15268deef39a07c417c2c11b92f6db
Flowaria <wnsghgj1234@naver.com>
2025-02-24 22:31:02 +0900

    Added OnBeforeBuildBatch and OnAfterBuildBatch

---
+++ GTFO-API/Patches/LevelGen_Patches.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using LevelGeneration;
+
+namespace GTFO.API.Patches
+{
+    [HarmonyPatch(typeof(LG_Factory))]
+    internal static class LevelGen_Patches
+    {
+        [HarmonyPrefix]
+        [HarmonyWrapSafe]
+        [HarmonyPatch(nameof(LG_Factory.NextBatch))]
+        private static void Pre_Batch(LG_Factory __instance)
+        {
+            if (__instance.m_batchStep > -1)
+            {
+                LevelAPI.AfterBuildBatch(__instance.m_currentBatchName);
+            }
+        }
+
+        [HarmonyPostfix]
+        [HarmonyWrapSafe]
+        [HarmonyPatch(nameof(LG_Factory.NextBatch))]
+        private static void Post_Batch(LG_Factory __instance)
+        {
+            LevelAPI.BeforeBuildBatch(__instance.m_currentBatchName);
+        }
+    }
+}
