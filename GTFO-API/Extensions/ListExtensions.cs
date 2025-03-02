3e6891d78163bc043a7b3bd608dd029704be7f0e
Flowaria <wnsghgj1234@naver.com>
2025-03-02 19:51:43 +0900

    Added ToManaged Extension

--- GTFO-API/Extensions/ListExtensions.cs
+++ GTFO-API/Extensions/ListExtensions.cs
@@ -20,5 +20,19 @@ namespace GTFO.API.Extensions
             }
             return il2cppList;
         }
+
+        /// <summary>
+        /// Converts the <see cref="Il2CppSystem.Collections.Generic.List{T}"/> to <see cref="List{T}"/>
+        /// </summary>
+        /// <returns>A copy of the list as <see cref="List{T}"/></returns>
+        public static List<T> ToManaged<T>(this Il2CppSystem.Collections.Generic.List<T> list)
+        {
+            List<T> managedList = new(list.Count);
+            for (int i = 0; i < list.Count; i++)
+            {
+                managedList.Add(list[i]);
+            }
+            return managedList;
+        }
     }
 }
