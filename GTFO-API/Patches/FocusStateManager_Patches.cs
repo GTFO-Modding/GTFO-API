68c9b6770d6c2594b4ee856f8ad9e10459554e7b
Flowaria <wnsghgj1234@naver.com>
2026-04-25 17:50:25 +0900

    Added OnGameStateChanged and OnFocusStateChanged Event

---
+++ GTFO-API/Patches/FocusStateManager_Patches.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+
+namespace GTFO.API.Patches;
+
+[HarmonyPatch(typeof(FocusStateManager))]
+internal static class FocusStateManager_Patches
+{
+    [HarmonyPatch(nameof(FocusStateManager.ChangeState))]
+    [HarmonyWrapSafe]
+    [HarmonyPostfix]
+    static void Post_StateChanged()
+    {
+        var old = FocusStateManager.Current.m_previousFocusState;
+        var current = FocusStateManager.Current.m_currentState;
+        if (old != current)
+        {
+            EventAPI.FocusChanged(old, current);
+        }
+    }
+}
