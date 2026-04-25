4f7ab573bf068b8153569ae10e52f4367c06fa91
Flowaria <wnsghgj1234@naver.com>
2026-04-25 17:05:34 +0900

    Added Extension Method for LG_LightInfo

---
+++ GTFO-API/Extensions/LightExtensions.cs
@@ -0,0 +1,20 @@
+﻿using GTFO.API.Components;
+using LevelGeneration;
+
+namespace GTFO.API.Extensions;
+
+/// <summary>
+/// Container for all extension for <see cref="LG_Light"/>
+/// </summary>
+public static class LightExtensions
+{
+    /// <summary>
+    /// Get <see cref="LG_LightInfo"/> Instance for given <see cref="LG_Light"/>
+    /// </summary>
+    /// <param name="light">Instance to check</param>
+    /// <returns>Instance of <see cref="LG_LightInfo"/> attached to Light</returns>
+    public static LG_LightInfo GetLightInfo(this LG_Light light)
+    {
+        return light.GetComponent<LG_LightInfo>();
+    }
+}
