b5a822c4e4188cfb08aad50d3602cab3854c68cd
Flowaria <wnsghgj1234@naver.com>
2026-09-20 00:27:19 +0900

    Added ConfigBinder Utility Class

---
+++ GTFO-API/Attributes/AcceptableValueAttribute.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using BepInEx.Configuration;
+
+namespace GTFO.API.Attributes
+{
+    /// <summary>
+    /// Base Type for <see cref="AcceptableValueRangeAttribute{T}"/> and <see cref="AcceptableValueListAttribute{T}"/>
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public abstract class AcceptableValueAttribute : Attribute
+    {
+        /// <summary>
+        /// <see cref="AcceptableValueBase"/> Instance that should be used for ConfigEntry
+        /// </summary>
+        public AcceptableValueBase Value { get; protected set; }
+    }
+
+    /// <summary>
+    /// Specify <see cref="AcceptableValueRange{T}"/> for ConfigEntry
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public sealed class AcceptableValueRangeAttribute<T> : AcceptableValueAttribute where T : IComparable
+    {
+        /// <summary>
+        /// Create Attribute Instance
+        /// </summary>
+        /// <param name="min">Allowed Minimum Value</param>
+        /// <param name="max">Allowed Maximum Value</param>
+        public AcceptableValueRangeAttribute(T min, T max)
+        {
+            Value = new AcceptableValueRange<T>(min, max);
+        }
+    }
+
+    /// <summary>
+    /// Specify <see cref="AcceptableValueList{T}"/> for ConfigEntry
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public sealed class AcceptableValueListAttribute<T> : AcceptableValueAttribute where T : IEquatable<T>
+    {
+        /// <summary>
+        /// Create Attribute Instance
+        /// </summary>
+        /// <param name="values">List of values that allowed</param>
+        public AcceptableValueListAttribute(params T[] values)
+        {
+            Value = new AcceptableValueList<T>(values);
+        }
+    }
+}
---
+++ GTFO-API/Attributes/ConfigEntryAttribute.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace GTFO.API.Attributes
+{
+    /// <summary>
+    /// Compounded Attribute for <see cref="DisplayNameAttribute"/>, <see cref="DescriptionAttribute"/> and <see cref="DefaultValueAttribute"/><br/>
+    /// - Key is <see cref="DisplayNameAttribute"/><br/>
+    /// - Description is <see cref="DescriptionAttribute"/><br/>
+    /// - DefaultValue is <see cref="DefaultValueAttribute"/><br/>
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public sealed class ConfigEntryAttribute : Attribute
+    {
+        /// <summary>
+        /// ConfigEntry Key Value
+        /// </summary>
+        public string Key { get; private set; }
+
+        /// <summary>
+        /// ConfigEntry Description Value
+        /// </summary>
+        public string Description { get; private set; }
+
+        /// <summary>
+        /// ConfigEntry Default Value
+        /// </summary>
+        public object DefaultValue { get; private set; }
+
+        /// <summary>
+        /// Create Attribute Instance
+        /// </summary>
+        /// <param name="key">Key Value</param>
+        /// <param name="defaultValue">Default Value</param>
+        /// <param name="description">Description Value</param>
+        public ConfigEntryAttribute(string key, object defaultValue, string description)
+        {
+            Key = key;
+            Description = description;
+            DefaultValue = defaultValue;
+        }
+
+        /// <summary>
+        /// Create Attribute Instance
+        /// </summary>
+        /// <param name="key">Key Value</param>
+        /// <param name="defaultValue">Default Value</param>
+        public ConfigEntryAttribute(string key, object defaultValue)
+        {
+            Key = key;
+            Description = null;
+            DefaultValue = defaultValue;
+        }
+    }
+}
