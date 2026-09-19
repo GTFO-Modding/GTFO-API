fb7bdfd3d9ea17530e6bf32dbb5c17c15a1e6260
Flowaria <wnsghgj1234@naver.com>
2026-09-20 00:32:13 +0900

    Add Extension Method for ConfigBinder.BindToType

---
+++ GTFO-API/Extensions/ConfigFileExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BepInEx.Configuration;
+using GTFO.API.Utilities;
+
+namespace GTFO.API.Extensions
+{
+    public static class ConfigFileExtensions
+    {
+        /// <inheritdoc cref="ConfigBinder.BindToType(ConfigFile, Type, bool)"/>
+        public static void BindToType(this ConfigFile config, Type type, bool searchNestedType = true)
+        {
+            ConfigBinder.BindToType(config, type, searchNestedType);
+        }
+
+        /// <inheritdoc cref="ConfigBinder.BindToType{T}(ConfigFile, bool)"/>
+        public static void BindToType<T>(this ConfigFile config, bool searchNestedType = true)
+        {
+            ConfigBinder.BindToType(config, typeof(T), searchNestedType);
+        }
+    }
+}
